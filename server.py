746015d54a8dea143c254f6e0641d30aa9c0ef87
engmaliksh-ux <engmalikmsh.1997@gmail.com>
2026-05-12 22:22:40 +0300

    clean server

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-"""
 import socket
 import os
 
@@ -6,11 +5,9 @@ host = "0.0.0.0"
 port = int(os.environ.get("PORT", 12345))
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 server.bind((host, port))
-
 server.listen()
 
 print("Server started... Waiting for connections")
@@ -37,49 +34,4 @@ while True:
             break
 
     client.close()
-
     print("Connection closed")
-"""
-
-import socket
-import os
-
-
-# إنشاء السيرفر
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# IP + PORT
-host = "0.0.0.0"
-port = int(os.environ.get("PORT", 12345))
-
-# ربط السيرفر
-server.bind((host, port))
-
-# تشغيل الاستماع
-server.listen()
-
-print("Server started... Waiting for connection")
-
-# انتظار اتصال
-client_socket, client_address = server.accept()
-
-print(f"Connected with: {client_address}")
-
-# استقبال وإرسال رسائل
-while True:
-    try:
-        msg = client_socket.recv(1024).decode()
-        if not msg:
-            break
-
-        print("Friend:", msg)
-
-        reply = input("You: ")
-        client_socket.send(reply.encode())
-
-    except:
-        print("Connection closed by client")
-        break
-
-client_socket.close()
-
