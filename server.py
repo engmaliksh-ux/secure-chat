fa36c6e806a962753dc47a73d65c367fed46aa20
engmaliksh-ux <engmalikmsh.1997@gmail.com>
2026-05-12 22:30:41 +0300

    fix render server loop

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+"""
 import socket
 import os
 
@@ -5,9 +6,11 @@ host = "0.0.0.0"
 port = int(os.environ.get("PORT", 12345))
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 server.bind((host, port))
+
 server.listen()
 
 print("Server started... Waiting for connections")
@@ -34,4 +37,49 @@ while True:
             break
 
     client.close()
+
     print("Connection closed")
+"""
+
+import socket
+import os
+
+
+# IP + PORT
+host = "0.0.0.0"
+port = int(os.environ.get("PORT", 12345))
+
+# إنشاء السيرفر
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# ربط السيرفر
+server.bind((host, port))
+
+# تشغيل الاستماع
+server.listen()
+
+print("Server started... Waiting for connection")
+
+# انتظار اتصال
+client_socket, client_address = server.accept()
+
+print(f"Connected with: {client_address}")
+
+# استقبال وإرسال رسائل
+while True:
+    try:
+        msg = client_socket.recv(1024).decode()
+        if not msg:
+            break
+
+        print("Friend:", msg)
+
+        reply = input("You: ")
+        client_socket.send(reply.encode())
+
+    except:
+        print("Connection closed by client")
+        break
+
+client_socket.close()
+
