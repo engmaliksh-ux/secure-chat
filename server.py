59a758695799b889eed34f9e3c75f8de66982e51
engmaliksh-ux <engmalikmsh.1997@gmail.com>
2026-05-12 21:47:54 +0300

    fix render server loop

--- server.py
+++ server.py
@@ -1,4 +1,40 @@
 import socket
+
+host = "0.0.0.0"
+port = 12345
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+print("Server started... Waiting for connections")
+
+while True:
+    client, address = server.accept()
+
+    print(f"Connected with: {address}")
+
+    while True:
+        try:
+            message = client.recv(1024).decode()
+
+            if not message:
+                break
+
+            print("Client:", message)
+
+            reply = f"Server received: {message}"
+
+            client.send(reply.encode())
+
+        except:
+            break
+
+    client.close()
+    print("Connection closed")
+
+"""
+import socket
 import os
 
 
@@ -39,3 +75,5 @@ while True:
         break
 
 client_socket.close()
+
+"""
